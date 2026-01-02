7493644920277474389c7afff2a8d89061f5546d
git-anshudubey <6152anshudubey@gmail.com>
2026-01-02 21:40:26 +0530

    Components added

---
+++ client/src/components/Container.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Container({children}){
+    return (
+        <div className="w-full max-auto px-4">
+            {children}
+        </div>
+    );
+}
+
+export default Container;
